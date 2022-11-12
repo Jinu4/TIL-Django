a6c4fa385e8a15ea25f5d0a2fccb7f2b61970dc0
jinu4 <antonyjinu.09@gmail.com>
2022-11-13 01:35:47 +0530

    added media folder to images

--- TIL/urls.py
+++ TIL/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf.urls.static import static
+from django.conf import settings
 from feed import urls as feed_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include(feed_urls, namespace='feed')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
