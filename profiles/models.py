d33c147ff2c2620bbcd2a56d0307d1933edcfca2
jinu4 <antonyjinu.09@gmail.com>
2022-11-13 01:22:48 +0530

    Added new thumbnail package&image field to profile

--- profiles/models.py
+++ profiles/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from sorl.thumbnail import ImageField
 
 
 class Profile(models.Model):
+    image = ImageField(upload_to='profiles')
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
