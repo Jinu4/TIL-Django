b5d33f13e3adb6140d2cfcbc9dc077e03d4b18eb
jinu4 <antonyjinu.09@gmail.com>
2022-11-15 18:23:34 +0530

    created post view

--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 
 
@@ -16,3 +17,20 @@ class PostDetailView(DetailView):
     template_name = 'feed/detail.html'
     model = Post
     context_object_name = 'post'
+
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    template_name = 'feed/create.html'
+    model = Post
+    fields = ['text']
+    success_url = '/'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+        obj.save()
+        return super().form_valid(form)
