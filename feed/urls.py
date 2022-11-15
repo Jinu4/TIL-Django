047e35d8613ba05b705ebbb4c5062ccf7f23acd2
jinu4 <antonyjinu.09@gmail.com>
2022-11-15 23:23:07 +0530

    Added a tailwind comp for post

--- feed/urls.py
+++ feed/urls.py
@@ -6,6 +6,5 @@ app_name = 'feed'
 urlpatterns = [
     path('', views.PostView.as_view(), name='homepage'),
     path('<int:pk>/', views.PostDetailView.as_view(), name='detail'),
-    path('new_post/', views.CreatePostView.as_view(), name='new_post')
-
+    path('new_post/', views.CreatePostView.as_view(), name='new_post'),
 ]
