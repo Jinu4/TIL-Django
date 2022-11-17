971ef1cc3d1a608d36678f0030f38b92adaea339
jinu4 <antonyjinu.09@gmail.com>
2022-11-17 20:26:20 +0530

    Created a view for profile

--- TIL/urls.py
+++ TIL/urls.py
@@ -19,9 +19,12 @@ from django.conf.urls import include
 from django.conf.urls.static import static
 from django.conf import settings
 from feed import urls as feed_urls
+from profiles import urls as profiles_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include(feed_urls, namespace='feed')),
+    path('', include(profiles_urls, namespace='profiles')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
